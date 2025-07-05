1c88c4fbfb5720749202576c4b8f860e26388bc4
nirishabhattarai1910 <nirishaaa.bhattarai@gmail.com>
2025-07-06 00:40:42 +0545

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+ const hamburger= document.querySelector(".hamburger");
+ const navlinks= document.querySelector(".navlinks");
+
+ hamburger.addEventListener("click",(e)=>{
+    navlinks.classList.toggle("active");
+ })
+
+
+
+ 
